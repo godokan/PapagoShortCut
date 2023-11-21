1cccfa4b32295c337367ff64330fb31046befef7
godokan <117326245+godokan@users.noreply.github.com>
2023-11-22 00:02:01 +0900

    Initial Commit

---
+++ PapagoShortCut/PapagoShortCutApp.swift
@@ -0,0 +1,17 @@
+//
+//  PapagoShortCutApp.swift
+//  PapagoShortCut
+//
+//  Created by 구자민 on 2023/11/22.
+//
+
+import SwiftUI
+
+@main
+struct PapagoShortCutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
