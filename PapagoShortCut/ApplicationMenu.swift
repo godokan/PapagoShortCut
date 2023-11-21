ea08e46505890f57a19443511f87631571f2cca7
godokan <117326245+godokan@users.noreply.github.com>
2023-11-22 02:33:20 +0900

    Initial Commit

---
+++ PapagoShortCut/ApplicationMenu.swift
@@ -0,0 +1,8 @@
+//
+//  ApplicationMenu.swift
+//  PapagoShortCut
+//
+//  Created by 구자민 on 2023/11/22.
+//
+
+import Foundation
---
+++ PapagoShortCut/WebView.swift
@@ -0,0 +1,8 @@
+//
+//  WebView.swift
+//  PapagoShortCut
+//
+//  Created by 구자민 on 2023/11/22.
+//
+
+import Foundation
